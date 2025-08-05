c21cc95daa716579f7451811d2ba35c2a57b498a
张文学 <1257545592@qq.com>
2025-08-05 16:04:52 +0800

    --

--- app/src/main/java/com/homecare/vod/MainActivity.kt
+++ app/src/main/java/com/homecare/vod/MainActivity.kt
@@ -37,36 +37,6 @@ class MainActivity : ComponentActivity() {
                     Button(modifier = Modifier.padding(innerPadding), onClick = {
 //                      Home
                         HomeCareVodSDK.onHangup = {}
-                        HomeCareVodSDK.call(
-                            token = "14E9FA067B350997836938946055A9BCA483740CC54EB29E6642D065CADCF5D8",
-                            "1848207774300024832",
-                            "1848207774300024832",
-                            symptomsDescription = "头疼",
-                            fileList = listOf(
-                                "https://upload-images.jianshu.io/upload_images/5809200-48dd99da471ffa3f.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240",
-                                "https://upload-images.jianshu.io/upload_images/5809200-7fe8c323e533f656.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240"
-                            ),
-                            true,
-
-                        )
-
-//                        HomeCareVodSDK.callWithJwt(
-//                            jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJoYWllclJvYm90IiwibmFtZSI6Iua1t-WwlOacuuWZqOS6uiIsImlhdCI6MTc1MzE2NTY1OX0.Xr_othyi9HBMoOnrr758VG6PCwXZ7jX24jxvpVUuxJE",
-//                            "1848207774300024832",
-//                            "1848207774300024832",
-//                            symptomsDescription = "测试症状描述",
-//                            fileList = listOf(
-//                                "http://e.hiphotos.baidu.com/image/pic/item/a1ec08fa513d2697e542494057fbb2fb4316d81e.jpg",
-//                                "http://c.hiphotos.baidu.com/image/pic/item/30adcbef76094b36de8a2fe5a1cc7cd98d109d99.jpg"
-//                            ), hasEcg = true,
-//                            onHangup = {
-//                                Toast.makeText(
-//                                    this@MainActivity,
-//                                    "挂断成功:$it",
-//                                    Toast.LENGTH_SHORT
-//                                ).show()
-//                            }
-//                        )
                     }) {
                         Text("视频")
                     }
